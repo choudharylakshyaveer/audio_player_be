cb5456578989256c324d1ffcf574d3825e6ee63e
choudharylakshyaveer <choudharylakshyaveer@gmail.com>
2025-09-25 14:03:35 +0530

    Albums API functionality addition

--- src/main/java/org/audio/player/controller/AudioController.java
+++ src/main/java/org/audio/player/controller/AudioController.java
@@ -1,6 +1,9 @@
 package org.audio.player.controller;
 
+import lombok.AllArgsConstructor;
+import org.audio.player.dto.AlbumsDTO;
 import org.audio.player.entity.AudioTrack;
+import org.audio.player.service.AlbumsService;
 import org.audio.player.service.MetadataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Set;
 
 
+@AllArgsConstructor
 @RestController
 public class AudioController {
 
-    @Autowired
     MetadataService metadataService;
+
+    AlbumsService albumsService;
 
     @GetMapping("/saveTrackMetadata")
     public ResponseEntity<Set<AudioTrack>> saveTrackMetadata(){
         Set<AudioTrack> audioTracks = metadataService.saveMetadata();
         return ResponseEntity.ok(audioTracks);
+    }
+
+    @GetMapping("/albums")
+    public ResponseEntity<Set<AlbumsDTO>> getAlbums(){
+        return ResponseEntity.ok(albumsService.getAlbums());
     }
 }
