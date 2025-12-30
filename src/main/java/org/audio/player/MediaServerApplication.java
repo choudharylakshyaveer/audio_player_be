43bf28738785abe8416d7d5fd0e5d626cfc67a22
choudharylakshyaveer <choudharylakshyaveer@gmail.com>
2025-12-31 00:59:46 +0530

    * signed URLs integration for media playback.

--- src/main/java/org/audio/player/MediaServerApplication.java
+++ src/main/java/org/audio/player/MediaServerApplication.java
@@ -2,6 +2,7 @@ package org.audio.player;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
